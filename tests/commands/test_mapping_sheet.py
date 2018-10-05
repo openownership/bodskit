b77dd474776c3dfe58d5f78a67eb7fa85ab99761
James Baster <james.baster@opendataservices.coop>
2018-10-05 16:35:48 +0100

    mapping-sheet - put codelist values in when enum is in an array

--- tests/commands/test_mapping_sheet.py
+++ tests/commands/test_mapping_sheet.py
@@ -18,3 +18,17 @@ def test_command():
 
     assert actual.getvalue() == \
         read_fixture('mapping-sheet-person-statement.csv').replace('\n', '\r\n')  # not sure why
+
+
+def test_enum_in_list():
+    """Special test for problem reported with sourceType checklist - values where not being reported"""
+
+    input_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                                  '..', 'fixtures', 'mapping-sheet-enum-in-list-input.json')
+
+    with patch('sys.stdout', new_callable=StringIO) as actual:
+        mapping_sheet = bodskit.mapping_sheet.MappingSheet(input_filename=input_filename)
+        mapping_sheet.handle()
+
+    assert actual.getvalue() == \
+        read_fixture('mapping-sheet-enum-in-list-output.csv').replace('\n', '\r\n')  # not sure why
